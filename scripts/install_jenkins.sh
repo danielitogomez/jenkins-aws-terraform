c11694be1549accb7e001e61e7bf0deef730c4d2
danielitogomez <daniel.gomez.amaran@gmail.com>
2021-01-26 00:33:37 -0300

    changes script

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -31,3 +31,16 @@ yum install jenkins java-1.8.0-openjdk -y
 echo "Step:3 Start and Enable Jenkins Service"
 systemctl start jenkins
 systemctl enable jenkins
+
+echo "Step:4 install Firewall"
+sudo yum install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+
+echo "Step:5 Open the ports 80 and 8080 in OS firewall"
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --reload
+
+echo "View the credentials"
+# cat /var/lib/jenkins/secrets/initialAdminPassword
