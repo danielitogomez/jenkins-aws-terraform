04f8a8b541e94df743c456133f3ca4ed6fbd18b6
danielitogomez <daniel.gomez.amaran@gmail.com>
2021-01-31 23:53:31 -0300

    Fix Virtualhost

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -7,11 +7,17 @@ echo "Configuring VirtualHost"
 
 sudo touch /etc/httpd/conf.d/00jenkins.conf && sudo chmod 766 /etc/httpd/conf.d/00jenkins.conf
 echo "<Virtualhost *:80>
-            ServerName        ec2-3-230-145-30.compute-1.amazonaws.com
-            ProxyRequests     On
-            ProxyPreserveHost On
-            AllowEncodedSlashes NoDecode
-        ProxyPass         /  http://localhost:8080/
+    ServerName        ec2-3-230-145-30.compute-1.amazonaws.com
+    ProxyRequests     Off
+    ProxyPreserveHost On
+    AllowEncodedSlashes NoDecode
+    <Proxy http://localhost:8080/*>
+      Order deny,allow
+      Allow from all
+    </Proxy>
+    ProxyPass         /  http://localhost:8080/ nocanon
+    ProxyPassReverse  /  http://localhost:8080/
+    ProxyPassReverse  /  http://ec2-3-230-145-30.compute-1.amazonaws.com/
 </Virtualhost>" > /etc/httpd/conf.d/00jenkins.conf
 
 if ! echo -e /etc/httpd/conf.d/00jenkins.conf; then
